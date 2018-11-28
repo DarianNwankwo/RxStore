29df6c6616887ba718aea935a51e722f32b9e0b5
DarianNwankwo <dnwankwo404@gmail.com>
2018-11-28 15:39:23 -0500

    Initial structure for app models

--- app/models.py
+++ app/models.py
@@ -1,23 +1,44 @@
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import app, db, login
+from app import app, db#, login
 
 
-class Patient(UserMixin, db.Model):
+class Person(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(32), index=True, unique=False)
     middle_name = db.Column(db.String(32), index=True, unique=False)
     last_name = db.Column(db.String(32), index=True, unique=False)
+    birthdate = db.Column(db.Date)
+    gender = db.Column(db.String(1), index=True)
 
 
-class Contact(db.Model):
+
+class Patient(Person):
+    pass
+    
+
+
+class Prescription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    patient_id = db.Column(db.Integer, db.ForeignKey("patient.id"))
-    street_address
-    state
-    city = db.Column(db.String(74), index=True, unique=True)
-    zip_code = db.Column(db.String(10), index=True)
-    country = db.Column(db.String(74), index=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    phone_number
+
+
+class Doctor(Person):
+    pass
+
+
+class Pharmacist(Person):
+    pass
+
+
+
+# class Contact(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     patient_id = db.Column(db.Integer, db.ForeignKey("patient.id"))
+#     street_address
+#     state
+#     city = db.Column(db.String(74), index=True, unique=True)
+#     zip_code = db.Column(db.String(10), index=True)
+#     country = db.Column(db.String(74), index=True)
+#     email = db.Column(db.String(120), index=True, unique=True)
+#     phone_number
