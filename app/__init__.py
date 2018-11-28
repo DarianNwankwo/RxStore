ec8a45f9be202e268c63e00e9801ffe30b31a877
DarianNwankwo <dnwankwo404@gmail.com>
2018-11-28 15:38:06 -0500

    Added support for database and db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask, request
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"
+login.login_message = "Please log in to access this page."
 bootstrap = Bootstrap(app)
 
-from app import routes
+from app import routes, models
