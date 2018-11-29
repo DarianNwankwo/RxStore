48ccc96f628dd844bb2ae580d1b6d3aa9f3d492b
DarianNwankwo <dnwankwo404@gmail.com>
2018-11-29 15:07:41 -0500

    Created user login form

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
 from app.models import Patient, Doctor, Pharmacist, Prescription
 
 
 class LoginForm(FlaskForm):
-    pass
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    user_type = SelectField(
+        "Type",
+        description="What type of user are you?", 
+        choices=[("patient", "Patient"), ("doctor", "Doctor"), ("pharmacist", "Pharmacist")],
+        validators=[DataRequired()]
+        )
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
 
 
 class RegistrationForm(FlaskForm):
