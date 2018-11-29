402a8c7373b87c7499d9d848922da57858fba07a
DarianNwankwo <dnwankwo404@gmail.com>
2018-11-29 08:58:23 -0500

    Change db creation to top level directory

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = f"{basedir}/databases"
+db_path = basedir + "/databases"
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "rxstore-like-no-other-baby-but-seriously-set-a-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or f"sqlite:///{os.path.join(db_path)}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
